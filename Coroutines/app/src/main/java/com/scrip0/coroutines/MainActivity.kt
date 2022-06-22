97dbe032e22138a6573b13151bccaadc3037122d
Scrip0 <lavrenenko.2003@gmail.com>
2022-06-22 12:03:20 -0400

    Added simple coroutine

--- Coroutines/app/src/main/java/com/scrip0/coroutines/MainActivity.kt
+++ Coroutines/app/src/main/java/com/scrip0/coroutines/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.scrip0.coroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+	val TAG = "MainActivity"
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		GlobalScope.launch {
+			delay(3000L)
+			Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+		}
+		Log.d(TAG, "Hello from thread here ${Thread.currentThread().name}")
+	}
 }
