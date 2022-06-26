1ebbd677777c388a775bd14cf73edc52c07607b7
Scrip0 <lavrenenko.2003@gmail.com>
2022-06-25 21:39:49 -0400

    Setup retrofit

---
+++ MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "7ab113c6e2cd4f93882e5ac61258eefe"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
