508f8f9c5220c3f642e49da122fa2dd0e345bbfa
Scrip0 <lavrenenko.2003@gmail.com>
2022-06-26 00:13:26 -0400

    Added search news feature

--- MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "7ab113c6e2cd4f93882e5ac61258eefe"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
