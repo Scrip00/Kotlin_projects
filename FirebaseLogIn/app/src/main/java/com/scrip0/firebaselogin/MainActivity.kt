3de6fc44ed164000e7332e9136cec119f2227556
Scrip0 <lavrenenko.2003@gmail.com>
2022-06-28 19:51:48 -0400

    Added google sign in feature

--- FirebaseLogIn/app/src/main/java/com/scrip0/firebaselogin/MainActivity.kt
+++ FirebaseLogIn/app/src/main/java/com/scrip0/firebaselogin/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.scrip0.firebaselogin
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -12,6 +20,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+
+const val REQUEST_CODE_SIGN_IN = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +43,56 @@ class MainActivity : AppCompatActivity() {
 		btnUpdateProfile.setOnClickListener {
 			updateProfile()
 		}
+
+		btnLogout.setOnClickListener {
+			auth.signOut()
+			checkLoggedInState()
+		}
+
+		btnSignInGoogle.setOnClickListener {
+			val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				.requestIdToken(getString(R.string.webclient_id))
+				.requestEmail()
+				.requestProfile()
+				.build()
+
+			val signInClient = GoogleSignIn.getClient(this, options)
+			signInClient.signInIntent.also {
+				getGoogleSignIn.launch(it)
+			}
+		}
 	}
+
+	private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+		val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+		CoroutineScope(Dispatchers.IO).launch {
+			try {
+				auth.signInWithCredential(credentials).await()
+				withContext(Dispatchers.Main) {
+					checkLoggedInState()
+					Toast.makeText(
+						this@MainActivity,
+						"Successfully logged in with Google",
+						Toast.LENGTH_LONG
+					).show()
+				}
+			} catch (e: Exception) {
+				withContext(Dispatchers.Main) {
+					Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+				}
+			}
+		}
+	}
+
+	private val getGoogleSignIn =
+		registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+			if (it.resultCode == Activity.RESULT_OK) {
+				val account = GoogleSignIn.getSignedInAccountFromIntent(it.data).result
+				account?.let {
+					googleAuthForFirebase(it)
+				}
+			}
+		}
 
 	private fun updateProfile() {
 		auth.currentUser?.let { user ->
@@ -108,14 +167,18 @@ class MainActivity : AppCompatActivity() {
 		}
 	}
 
+	@SuppressLint("SetTextI18n")
 	private fun checkLoggedInState() {
 		val user = auth.currentUser
 		if (user == null) {
 			tvLoggedIn.text = "You are not logged in"
+			etUsername.setText("")
+			ivProfilePicture.setImageURI(null)
 		} else {
 			tvLoggedIn.text = "You are logged in"
 			etUsername.setText(user.displayName)
 			ivProfilePicture.setImageURI(user.photoUrl)
+			Log.d("LOLPP", user.photoUrl.toString())
 		}
 	}
 }
