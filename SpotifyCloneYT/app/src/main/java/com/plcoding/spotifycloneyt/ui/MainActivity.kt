4041dd990e8a40342ea754376cf1f08d428e5dac
Scrip0 <lavrenenko.2003@gmail.com>
2022-07-10 21:16:10 -0400

    Updated song fragment

--- SpotifyCloneYT/app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
+++ SpotifyCloneYT/app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.plcoding.spotifycloneyt.ui
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -23,132 +24,135 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel: MainViewModel by viewModels()
+	private val mainViewModel: MainViewModel by viewModels()
 
-    @Inject
-    lateinit var swipeSongAdapter: SwipeSongAdapter
+	@Inject
+	lateinit var swipeSongAdapter: SwipeSongAdapter
 
-    @Inject
-    lateinit var glide: RequestManager
+	@Inject
+	lateinit var glide: RequestManager
 
-    private var curPlayingSong: Song? = null
+	private var curPlayingSong: Song? = null
 
-    private var playbackState: PlaybackStateCompat? = null
+	private var playbackState: PlaybackStateCompat? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        subscribeToObservers()
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		subscribeToObservers()
 
-        vpSong.adapter = swipeSongAdapter
+		vpSong.adapter = swipeSongAdapter
 
-        vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                if (playbackState?.isPlaying == true) {
-                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
-                } else {
-                    curPlayingSong = swipeSongAdapter.songs[position]
-                }
-            }
-        })
+		vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+			override fun onPageSelected(position: Int) {
+				super.onPageSelected(position)
+				if (playbackState?.isPlaying == true) {
+					mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+				} else {
+					curPlayingSong = swipeSongAdapter.songs[position]
+				}
+			}
+		})
 
-        ivPlayPause.setOnClickListener {
-            curPlayingSong?.let {
-                mainViewModel.playOrToggleSong(it, true)
-            }
-        }
+		ivPlayPause.setOnClickListener {
+			curPlayingSong?.let {
+				mainViewModel.playOrToggleSong(it, true)
+			}
+		}
 
-        swipeSongAdapter.setOnClickListener {
-            navHostFragment.findNavController().navigate(
-                R.id.globalActionToSongFragment
-            )
-        }
+		swipeSongAdapter.setOnClickListener {
+			navHostFragment.findNavController().navigate(
+				R.id.globalActionToSongFragment
+			)
+		}
+	}
 
-        navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.songFragment -> hideBottomBar()
-                R.id.homeFragment -> showBottomBar()
-                else -> showBottomBar()
-            }
-        }
-    }
+	override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+		super.onPostCreate(savedInstanceState, persistentState)
+		navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+			when (destination.id) {
+				R.id.songFragment -> hideBottomBar()
+				R.id.homeFragment -> showBottomBar()
+				else -> showBottomBar()
+			}
+		}
+	}
 
-    private fun hideBottomBar() {
-        ivCurSongImage.isVisible = false
-        vpSong.isVisible = false
-        ivPlayPause.isVisible = false
-    }
+	private fun hideBottomBar() {
+		ivCurSongImage.isVisible = false
+		vpSong.isVisible = false
+		ivPlayPause.isVisible = false
+	}
 
-    private fun showBottomBar() {
-        ivCurSongImage.isVisible = true
-        vpSong.isVisible = true
-        ivPlayPause.isVisible = true
-    }
+	private fun showBottomBar() {
+		ivCurSongImage.isVisible = true
+		vpSong.isVisible = true
+		ivPlayPause.isVisible = true
+	}
 
-    private fun switchViewPagerToCurrentSong(song: Song) {
-        val newItemIndex = swipeSongAdapter.songs.indexOf(song)
-        if (newItemIndex != -1) {
-            vpSong.currentItem = newItemIndex
-            curPlayingSong = song
-        }
-    }
+	private fun switchViewPagerToCurrentSong(song: Song) {
+		val newItemIndex = swipeSongAdapter.songs.indexOf(song)
+		if (newItemIndex != -1) {
+			vpSong.currentItem = newItemIndex
+			curPlayingSong = song
+		}
+	}
 
-    private fun subscribeToObservers() {
-        mainViewModel.mediaItems.observe(this) {
-            it?.let { result ->
-                when (result.status) {
-                    SUCCESS -> {
-                        result.data?.let { songs ->
-                            swipeSongAdapter.songs = songs
-                            if (songs.isNotEmpty()) {
-                                glide.load((curPlayingSong ?: songs[0]).imageUrl)
-                                    .into(ivCurSongImage)
-                            }
-                            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
-                        }
-                    }
-                    ERROR -> Unit
-                    LOADING -> Unit
-                }
-            }
-        }
-        mainViewModel.curPlayingSong.observe(this) {
-            if (it == null) return@observe
+	private fun subscribeToObservers() {
+		mainViewModel.mediaItems.observe(this) {
+			it?.let { result ->
+				when (result.status) {
+					SUCCESS -> {
+						result.data?.let { songs ->
+							swipeSongAdapter.songs = songs
+							if (songs.isNotEmpty()) {
+								glide.load((curPlayingSong ?: songs[0]).imageUrl)
+									.into(ivCurSongImage)
+							}
+							switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+						}
+					}
+					ERROR -> Unit
+					LOADING -> Unit
+				}
+			}
+		}
+		mainViewModel.curPlayingSong.observe(this) {
+			if (it == null) return@observe
 
-            curPlayingSong = it.toSong()
-            glide.load(curPlayingSong?.imageUrl).into(ivCurSongImage)
-            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
-        }
-        mainViewModel.playbackState.observe(this) {
-            playbackState = it
-            ivPlayPause.setImageResource(
-                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
-            )
-        }
-        mainViewModel.isConnected.observe(this) {
-            it?.getContentIfNotHandled()?.let { result ->
-                when (result.status) {
-                    ERROR -> Snackbar.make(
-                        rootLayout,
-                        result.message ?: "An unknown error occurred",
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                    else -> Unit
-                }
-            }
-        }
-        mainViewModel.networkError.observe(this) {
-            it?.getContentIfNotHandled()?.let { result ->
-                when (result.status) {
-                    ERROR -> Snackbar.make(
-                        rootLayout,
-                        result.message ?: "An unknown error occurred",
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                    else -> Unit
-                }
-            }
-        }
-    }
+			curPlayingSong = it.toSong()
+			glide.load(curPlayingSong?.imageUrl).into(ivCurSongImage)
+			switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+		}
+		mainViewModel.playbackState.observe(this) {
+			playbackState = it
+			ivPlayPause.setImageResource(
+				if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+			)
+		}
+		mainViewModel.isConnected.observe(this) {
+			it?.getContentIfNotHandled()?.let { result ->
+				when (result.status) {
+					ERROR -> Snackbar.make(
+						rootLayout,
+						result.message ?: "An unknown error occurred",
+						Snackbar.LENGTH_LONG
+					).show()
+					else -> Unit
+				}
+			}
+		}
+		mainViewModel.networkError.observe(this) {
+			it?.getContentIfNotHandled()?.let { result ->
+				when (result.status) {
+					ERROR -> Snackbar.make(
+						rootLayout,
+						result.message ?: "An unknown error occurred",
+						Snackbar.LENGTH_LONG
+					).show()
+					else -> Unit
+				}
+			}
+		}
+	}
 }
