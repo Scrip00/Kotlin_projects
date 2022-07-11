01cbdf0a608dde61f08bc30d0872cc4392c57484
Scrip0 <lavrenenko.2003@gmail.com>
2022-07-11 03:58:18 -0400

    Looked into modifiers

--- ComposePlayground/Lesson_1/app/src/main/java/com/scrip0/lesson_1/MainActivity.kt
+++ ComposePlayground/Lesson_1/app/src/main/java/com/scrip0/lesson_1/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -20,15 +18,33 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			Column(
 				modifier = Modifier
-					.width(250.dp)
-					.fillMaxHeight(0.7f)
-					.background(Color.Green),
-				horizontalAlignment = Alignment.CenterHorizontally,
-				verticalArrangement = Arrangement.SpaceEvenly
+//					.width(300.dp)
+					.fillMaxHeight(0.5f)
+					.fillMaxWidth()
+					.background(Color.Green)
+//					.padding(top = 50.dp)
+//					.requiredWidth(600.dp) // Fixed width
+					.border(5.dp, Color.Magenta)
+					.padding(5.dp)
+					.border(5.dp, Color.Blue)
+					.padding(5.dp)
+					.border(10.dp, Color.Red)
+					.padding(10.dp) // Paddings applied sequentially
 			) {
-				Text("Hello")
+				Text(
+					"Hello",
+					modifier = Modifier.clickable {
+						// Do smth
+					}
+//						.offset(0.dp, 20.dp)
+//						.border(5.dp, Color.Yellow)
+//						.padding(5.dp)
+//						.offset(20.dp, 20.dp)
+//						.border(10.dp, Color.Black)
+//						.padding(10.dp)
+				)
+				Spacer(modifier = Modifier.height(50.dp))
 				Text("World")
-				Text("!!!")
 			}
 		}
 	}
