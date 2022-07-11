005e1d039a8cf361ab6e9f1bbc46f26e0ffcb8d7
Scrip0 <lavrenenko.2003@gmail.com>
2022-07-11 03:27:43 -0400

    Went through columns/rows and texts

--- ComposePlayground/Lesson_1/app/src/main/java/com/scrip0/lesson_1/MainActivity.kt
+++ ComposePlayground/Lesson_1/app/src/main/java/com/scrip0/lesson_1/MainActivity.kt
@@ -3,26 +3,33 @@ package com.scrip0.lesson_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Greeting(name = "Phillip")
+			Column(
+				modifier = Modifier
+					.width(250.dp)
+					.fillMaxHeight(0.7f)
+					.background(Color.Green),
+				horizontalAlignment = Alignment.CenterHorizontally,
+				verticalArrangement = Arrangement.SpaceEvenly
+			) {
+				Text("Hello")
+				Text("World")
+				Text("!!!")
+			}
 		}
 	}
-}
-
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-	Greeting("Android")
 }
